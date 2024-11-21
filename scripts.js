2f22e0ffe0ec671b0dca8837fd7070513133af59
Washington Pereira <slash.o.teobaldo@gmail.com>
2024-11-20 21:05:59 -0300

    Firts Comit

---
+++ scripts.js
@@ -0,0 +1,11 @@
+let formulario = document.querySelector(".formulario")
+
+function aparecer() {
+
+    formulario.style.marginLeft="41%"
+}
+
+function desaparecer() {
+    formulario.style.marginLeft="-41%"
+    
+}
